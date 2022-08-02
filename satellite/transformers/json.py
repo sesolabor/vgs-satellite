de992c280f1b4ef38309b1b4f8aa41da94027f0a
Christophe Prakash <christophe.prakash@sesolabor.com>
2022-08-01 17:22:49 -0700

    Fix revealing multiple values

--- satellite/transformers/json.py
+++ satellite/transformers/json.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from typing import Callable
 
 from jsonpath_ng import parse
@@ -11,6 +12,15 @@ class JsonTransformer(Transformer):
         payload_json = json.loads(payload)
         for expression in self.config.array:
             json_expr = parse(expression)
+            transformed_values = []
             for match in json_expr.find(payload_json):
-                json_expr.update(payload_json, operation(match.value))
+                transformed_value = operation(match.value)
+                transformed_values.append(transformed_value)
+
+                json_expr.update(payload_json, transformed_value)
+
+            # Hack to handle revealing arrays of multiple tokens
+            if str(json_expr) == "$.revealTokens.[*]":
+                whole_array_expr = parse("$.revealTokens")
+                whole_array_expr.update(payload_json, transformed_values)
         return json.dumps(payload_json)
